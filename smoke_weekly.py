386ef4f45af9c6c6e94b94bb03a5aabc27036859
Misiek87 <32161269+Misiek87@users.noreply.github.com>
2017-09-21 12:15:59 +0200

    input + sample folder iterator

---
+++ smoke_weekly.py
@@ -0,0 +1,17 @@
+import os
+import matplotlib
+
+start = input("Please input the first Changelist to be processed. ")
+end = input("Please input the last Changelist to be processed. ")
+
+directory = os.fsencode("C:/b/")
+
+for file in os.listdir(directory):
+    filename = os.fsdecode(file)
+    if filename.endswith(".csv"):
+        time = os.path.getctime(directory + file)
+        print(time)
+        print(filename)
+        continue
+    else:
+        continue
