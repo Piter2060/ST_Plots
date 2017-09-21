1452df6d82b0e0c3fb9cd8b75e35c9dc281250b3
Piter2060 <piotrwojcicki1996@gmail.com>
2017-09-21 15:24:20 +0200

    0.0.2 Lists of CL's and Directories

--- smoke_weekly.py
+++ smoke_weekly.py
@@ -1,14 +1,22 @@
 import os, glob
 import matplotlib
 #basic input
+"""
 start = input("Please input the first Changelist to be processed. ")
-end = input("Please input the last Changelist to be processed. ")
+end = input("Please input the last Changelist to be processed. ")"""
+start = 1
+end = 9999999999999
 directory = r"C:/Area/**/"
 path =  directory + 'Smoke Report General - AOI 200.xlsx'
+cl = []
+directories = []
 for file in glob.glob(path):
     dirname = os.fsdecode(directory)
     if (int(file[8:14:]) >= int(start) and int(file[8:14:]) <= int(end)):
         print(file)
+        directories.append(file)
         print(file[8:14:])
-    #nie wyświetla niczego 
-    
+        cl.append(int(file[8:14:]))
+
+print(directories)
+print(cl)
