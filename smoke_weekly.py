c1c00aa9fed8a95bfc2a3191393bec1d42873f50
Misiek87 <32161269+Misiek87@users.noreply.github.com>
2017-09-21 15:51:21 +0200

    working input

--- smoke_weekly.py
+++ smoke_weekly.py
@@ -1,16 +1,14 @@
 import os, glob
 import matplotlib
 #basic input
-start = 444437
-#input("Please input the first Changelist to be processed. ")
-end = 454777
-#input("Please input the last Changelist to be processed. ")
+start = input("Please input the first Changelist to be processed. ")
+end = input("Please input the last Changelist to be processed. ")
 directory = r"C:/Area/**/"
 path =  directory + 'Smoke Report General - AOI 200.xlsx'
 for file in glob.glob(path):
     dirname = os.fsdecode(directory)
-    print(file)
-    print(file[8:14:])
+    if (int(file[8:14:]) >= int(start) and int(file[8:14:]) <= int(end)):
+        print(file)
+        print(file[8:14:])
     #nie wyświetla niczego 
-    """if (int(file[8:14:]) >= start and int(file[8:14:]) <= end):
-        print(directory)"""
+    
