07f5518995282d99321e044694b5a8cca121259c
Piter2060 <piotrwojcicki1996@gmail.com>
2017-09-21 15:03:03 +0200

    0.0.1 Finding Smokes CL and Directory

--- smoke_weekly.py
+++ smoke_weekly.py
@@ -1,14 +1,16 @@
-import os
+import os, glob
 import matplotlib
 #basic input
 start = 444437
 #input("Please input the first Changelist to be processed. ")
 end = 454777
 #input("Please input the last Changelist to be processed. ")
-
-direct = "C:/Area/"
-
-for directory in os.listdir(direct):
+directory = r"C:/Area/**/"
+path =  directory + 'Smoke Report General - AOI 200.xlsx'
+for file in glob.glob(path):
     dirname = os.fsdecode(directory)
-    if os.path.isdir(int(dirname) >= start and int(dirname) <= end):
-        print(directory)
+    print(file)
+    print(file[8:14:])
+    #nie wyświetla niczego 
+    """if (int(file[8:14:]) >= start and int(file[8:14:]) <= end):
+        print(directory)"""
