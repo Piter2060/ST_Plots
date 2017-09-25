bca976dfcd76215964ff5b8884758448ac37ca86
Misiek87 <32161269+Misiek87@users.noreply.github.com>
2017-09-25 10:07:53 +0200

    smoke_weekly plotting
    ready to go

--- smoke_weekly.py
+++ smoke_weekly.py
@@ -1,6 +1,7 @@
 import os, glob
 import pandas as pd
-import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 #basic input
 """
 start = input("Please input the first Changelist to be processed. ")
@@ -8,20 +9,58 @@ end = input("Please input the last Changelist to be processed. ")"""
 start = 1
 end = 9999999999999
 directory = r"C:/Area/**/"
-path =  directory + 'Smoke Report General - AOI 200.xlsx'
+path =  directory + 'Javelin Smoke Report General - AOI 200.xlsx'
 cl = []
 directories = []
-for file in glob.glob(path):
-    dirname = os.fsdecode(directory)
-    if (int(file[8:14:]) >= int(start) and int(file[8:14:]) <= int(end)):
-        #print(file)
-        directories.append(file)
-        #print(file[8:14:])
-        cl.append(int(file[8:14:]))
-        df = pd.read_excel(open(file, 'rb'), sheetname=0)
-        print(df.iloc[9,6])
 
-#Dasd
+Pass = []
+Fail = []
+Issues = []
+Blocked = []
 
-print(directories)
-print(cl)
+def resultSearch():
+    for file in glob.glob(path):
+        dirname = os.fsdecode(directory)
+        if (int(file[8:14:]) >= int(start) and int(file[8:14:]) <= int(end)):
+            directories.append(file)
+            cl.append(int(file[8:14:]))
+            df = pd.read_excel(open(file, 'rb'), sheetname=0)
+            Pass.append(df.iloc[3, 7])
+            Fail.append(df.iloc[4, 7])
+            Issues.append(df.iloc[5, 7])
+            Blocked.append(df.iloc[6, 7])
+            
+
+
+def resPlotter():
+    global Pass
+    global Fail
+    global Issues
+    global Blocked
+    Pass = np.array(Pass)
+    Fail = np.array(Fail)
+    Issues = np.array(Issues)
+    Blocked = np.array(Blocked)
+    N = len(cl)
+    ind = np.arange(N)  # the x locations for the groups
+    width = 0.35  # the width of the bars: can also be len(x) sequence
+    
+    p1 = plt.bar(ind, Pass, width, color='green')
+    p2 = plt.bar(ind, Fail, width, color='red', bottom = Pass)
+    p3 = plt.bar(ind, Issues, width, color='yellow', bottom = Pass + Fail)
+    p4 = plt.bar(ind, Blocked, width, color='black', bottom = Pass + Fail + Issues)
+    
+    plt.ylabel('Results')
+    plt.title('Weekly Smoke Report')
+    plt.xticks(ind, (cl))
+    plt.yticks(np.arange(0, 11, 1))
+    plt.legend((p1[0], p2[0], p3[0], p4[0]), ('Pass', 'Fail', 'Issues', 'Blocked'))
+    
+    plt.show()
+    
+    
+resultSearch()
+resPlotter()
+
+
+
